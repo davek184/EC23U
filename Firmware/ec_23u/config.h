40f72db738ef92bb57a59f1967d60d493aaf19d0
Cipulot <40441626+Cipulot@users.noreply.github.com>
2022-11-12 00:42:07 +0100

    Firmware Dump

---
+++ Firmware/ec_23u/config.h
@@ -0,0 +1,64 @@
+/* Copyright 2022 Cipulot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 6
+
+/* Custom matrix pins and port select array */
+#define MATRIX_ROW_PINS \
+    { B13, B12, A7, B0 }
+#define MATRIX_COL_CHANNELS \
+    { 4, 6, 3, 2, 0, 1 }
+#define MUX_SEL_PINS \
+    { C14, C13, B6 }
+
+/* Hardware peripherals pins */
+#define APLEX_EN_PIN C15
+#define DISCHARGE_PIN A3
+#define ANALOG_PORT A2
+
+/* RGB Strip */
+#define RGB_DI_PIN B5
+#define RGBLED_NUM 15
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
+
+/* COL2ROW, ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+//#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#define DEBUG_MATRIX_SCAN_RATE
---
+++ Firmware/ec_23u/readme.md
@@ -0,0 +1,21 @@
+# EC23U
+
+Open source numpad Electrostatic Capacitive PCB.
+
+* Keyboard Maintainer: [cipulot](https://github.com/cipulot)
+* Hardware Supported: EC23U
+* Hardware Availability: TBD
+
+Make example for this keyboard (after setting up your build environment):
+
+    make ec_23u:default
+    make ec_23u:via
+    make ec_23u:vial
+
+Flashing example for this keyboard:
+
+    make ec_23u:default:flash
+    make ec_23u:via:flash
+    make ec_23u:vial:flash
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
