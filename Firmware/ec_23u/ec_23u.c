86c3163c4f76a3fd8961bb24857078cfe73b94ee
Cipulot <40441626+Cipulot@users.noreply.github.com>
2022-11-12 11:45:19 +0100

    Change of `led_state` check.

--- Firmware/ec_23u/ec_23u.c
+++ Firmware/ec_23u/ec_23u.c
@@ -23,7 +23,7 @@ void matrix_init_kb(void) {
 
 bool led_update_kb(led_t led_state) {
     if(led_update_user(led_state)) {
-        writePin(B14, led_state.caps_lock);
+        writePin(B14, led_state.num_lock);
     }
     return true;
 }
